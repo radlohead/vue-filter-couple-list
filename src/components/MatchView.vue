f3f8ec221ec4768259563566316ce71690354f2e
minho <radlohead@naver.com>
2019-03-22 20:34:09 +0900

    sass 적용

--- src/components/MatchView.vue
+++ src/components/MatchView.vue
@@ -1,8 +1,27 @@
 <template>
   <section>
-    MatchVue
+    <table class="match-list">
+      <thead>
+        <tr>
+          <th>matched</th>
+          <th>left</th>
+          <th>right</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>2</td>
+          <td>3</td>
+        </tr>
+      </tbody>
+    </table>
   </section>
 </template>
+
+<style lang="sass">
+  @import '../css/components/MatchView.scss';
+</style>
 
 <script>
 export default {
