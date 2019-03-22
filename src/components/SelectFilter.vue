0da838f343b49b192ae7bd7279a6a85b6011bf5c
minho <radlohead@naver.com>
2019-03-22 18:19:55 +0900

    selectFilter 선택시 value 받음

--- src/components/SelectFilter.vue
+++ src/components/SelectFilter.vue
@@ -1,16 +1,36 @@
 <template>
   <div>
     <button v-on:click="getHobbysList">SelectFilter</button>
+    <select
+      name="getHobbysListSelectFilter"
+      @change="getHobbysListSelectFilter"
+    >
+      <option disabled selected="selected">filter</option>
+      <option
+        v-for="item in getHobbysListSelectFilterAlphabet"
+        :key="item.text"
+      >{{ item.text }}</option>
+    </select>
   </div>
 </template>
 
 <script>
+import { mapGetters, mapActions } from 'vuex'
+
 export default {
   name: 'SelectFilter',
   methods: {
-    getHobbysList (e) {
-      this.$store.dispatch('getHobbysList', e)
+    getHobbysListSelectFilter (e) {
+      this.$store.commit('getHobbysListSelectFilter', e.target)
     }
+  },
+  computed: {
+    ...mapActions([
+      'getHobbysList'
+    ]),
+    ...mapGetters([
+      'getHobbysListSelectFilterAlphabet'
+    ])
   }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -8,6 +8,14 @@ const store = new Vuex.Store({
   state: {
     hobbysList: []
   },
+  getters: {
+    getHobbysListSelectFilterAlphabet () {
+      // console.log(JSON.parse(JSON.stringify(store.state.hobbysList)))
+      let result = []
+      for (let i = 65; i <= 90; i++) result.push({ text: String.fromCharCode(i) })
+      return result
+    }
+  },
   actions: {
     async getHobbysList ({ commit }) {
       const response = await axios.get('http://localhost:4000/hwahae/3')
@@ -19,6 +27,9 @@ const store = new Vuex.Store({
   mutations: {
     getHobbysList (state) {
       console.log(JSON.parse(JSON.stringify(state.hobbysList[0])))
+    },
+    getHobbysListSelectFilter (state, items) {
+      console.log('getHobbysListSelectFilter', items.value)
     }
   }
 })
