dd5a990494a42b18c52d16a164543d9c672b4e1f
minho <radlohead@naver.com>
2019-03-24 22:25:07 +0900

    common, define scss 추가

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,10 @@
   </div>
 </template>
 
+<style lang="sass">
+  @import './css/App.scss';
+</style>
+
 <script>
 import Home from './components/Home'
 
@@ -14,14 +18,3 @@ export default {
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
