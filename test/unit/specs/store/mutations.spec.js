3d1d70723b9aeaea189092dbbcbcc999cc99d536
minho <radlohead@naver.com>
2019-03-24 20:17:22 +0900

    mutations 테스트 코드 작성 완료

--- test/unit/specs/store/mutations.spec.js
+++ test/unit/specs/store/mutations.spec.js
@@ -1,4 +1,5 @@
 import 'array-flat-polyfill';
+import { shallow } from 'vue-test-utils';
 import mutations from '@/store/mutations';
 import hobbysList100 from '../../hobbysList100.json';
 
@@ -8,19 +9,36 @@ const mockState = {
     hobbysResultList: []
 }
 
-describe('mutations getHobbysList test', () => {
+const items = {
+    value: 'B'
+}
+
+describe('mutations getHobbysList', () => {
     mutations.getHobbysList(mockState);
 
-    it('state.hobbysList test', () => {
+    it('state.hobbysList', () => {
         expect(mockState.hobbysList[0]).toEqual({ id: 1, hobbys: 'ABCDNOPRSV' });
     });
 
-    it('state.hobbysMatchList matched test', () => {
+    it('state.hobbysMatchList matched', () => {
         expect(mockState.hobbysMatchList[0].matched).toBe('10-32');
     });
 
-    it('state.hobbysMatchList left, right test', () => {
+    it('state.hobbysMatchList left, right', () => {
         expect(mockState.hobbysMatchList[0].left).toBe('ABEHIMNQVY');
         expect(mockState.hobbysMatchList[0].right).toBe('ADEIMNOQVY');
+    });
+});
+
+describe('mutations handleChangeHobbysListFilter', () => {
+    mutations.handleChangeHobbysListFilter(mockState, items);
+    
+    it('state.hobbysResultList matched', () => {
+        expect(mockState.hobbysResultList[0].matched).toBe('15-18');
+    });
+
+    it('state.hobbysResultList left, right', () => {
+        expect(mockState.hobbysResultList[0].left).toBe('ABIJKLSWXZ');
+        expect(mockState.hobbysResultList[0].right).toBe('ABKLNQSWXZ');
     });
 });
