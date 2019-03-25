6aae95d9137a6950c013ebca8b23f8404c31df0a
minho <radlohead@naver.com>
2019-03-25 13:49:08 +0900

    async await -> jquery getJSON으로 변경

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,14 +1,12 @@
+import $ from 'jquery'
+
 export const HOBBYS_FILTER = 'HOBBYS_FILTER'
 
 export default {
-  async getHobbysList ({ commit }) {
-    try {
-      const hobbysResponse = await fetch('http://mynode2-env.q5kasmz2p2.ap-northeast-2.elasticbeanstalk.com/hwahae/100')
-      const hobbys = await hobbysResponse.json()
-      this.state.hobbysList.push(hobbys)
+  getHobbysList ({ commit }) {
+    $.getJSON('https://recurit.hwahae.co.kr/100', (response) => {
+      this.state.hobbysList.push(response)
       commit('getHobbysList')
-    } catch (err) {
-      console.log('hobbs to fetch fail', err)
-    }
+    })
   }
 }
