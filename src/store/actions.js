1b6a7b9e9ad7a14a4b554eb2cf6d83692ebc23cb
minho <radlohead@naver.com>
2019-03-25 10:10:07 +0900

    axios제거 및 fetch로 변경

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,12 +1,14 @@
-import axios from 'axios'
-
 export const HOBBYS_FILTER = 'HOBBYS_FILTER'
 
 export default {
   async getHobbysList ({ commit }) {
-    const response = await axios.get('http://mynode2-env.q5kasmz2p2.ap-northeast-2.elasticbeanstalk.com/hwahae/100')
-
-    this.state.hobbysList.push(response.data)
-    commit('getHobbysList')
+    try {
+      const hobbysResponse = await fetch('http://mynode2-env.q5kasmz2p2.ap-northeast-2.elasticbeanstalk.com/hwahae/100')
+      const hobbys = await hobbysResponse.json()
+      this.state.hobbysList.push(hobbys)
+      commit('getHobbysList')
+    } catch (err) {
+      console.log('hobbs to fetch fail', err)
+    }
   }
 }
